b7dec7b4520d0513e19ccddf1384eebce42cd068
Jeppe Lindberg <jepp3441@gmail.com>
2026-03-30 22:51:35 +0200

    progress

--- scripts/tile_information.gd
+++ scripts/tile_information.gd
@@ -1,7 +1,8 @@
 @tool
 extends Panel
 
-@export var vbox: VBoxContainer
+@export var scroll: ScrollContainer
+@export var first_child_of_scroll: MarginContainer
 
 @export var barracks_level_text: RichTextLabel
 
@@ -18,9 +19,11 @@ func _process(delta: float) -> void:
 	if not Engine.is_editor_hint():
 		if open:
 			expand_pct = min(expand_pct + (delta * 4.0), 1.0)
+			expand_pct = min(get_parent().size.y * expand_pct, first_child_of_scroll.size.y) / max(get_parent().size.y, first_child_of_scroll.size.y)
 		else:
 			expand_pct = max(expand_pct - (delta * 4.0), 0.0)
 			
 		barracks_level_text.text = str(barracks_level)
 
+	scroll.custom_minimum_size.y = first_child_of_scroll.size.y
 	custom_minimum_size.y = snapped(get_parent().size.y * expand_pct, 2.0)
--- scripts/ui_countup.gd
+++ scripts/ui_countup.gd
@@ -1,30 +1,26 @@
 @tool
-extends Node2D
-
-@export var turn_mgt:Node
+extends Panel
 
 @export_range(0.0,1.0) var visual_fill = 1.0
-@export var radial_sprites:Array[Sprite2D] = []
+@export var radial_textures:Array[TextureRect] = []
 
 var progress = 0.0
 
 
 func _process(delta: float) -> void:
-	var trigger_turn = false
 	if not Engine.is_editor_hint():
 		if progress < 1.0:
 			progress += delta * 0.5
 			if progress >= 1.0:
 				progress = 1.0
-				trigger_turn = true
 
 		visual_fill = progress
 
-	for sprite in radial_sprites:
-		sprite.material.set_shader_parameter('fill_ratio', visual_fill)
+		if progress >= 1.0:
+			progress = 0.0
 
-	if trigger_turn:
-		turn_mgt.accept_turn()
+	for texture in radial_textures:
+		texture.material.set_shader_parameter('fill_ratio', visual_fill)
 
 func start_timer():
 	progress = 0.0
